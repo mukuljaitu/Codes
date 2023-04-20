485ba98735632ee068c049d3e18375c80015849a
mukuljaitu <mukulgarg334@gmail.com>
2023-04-20 20:01:48 +0530

    Checked the code and error is at array assignment

--- Sample.c
+++ Sample.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
-
+#include <math.h>
 int main()
-{  
-    printf("AB\aCD\tEFGHIJKLMNOP\rQRSTU\bVWX\nYZ");
+{
+    int snumber,  number = 5100,ar[] = {};
+    /* First number */
+     int n = pow(10, 1);
+    // snumber = number % n;
+    // ar[1 - 1] = snumber * 10 / n;
+    // number -= snumber;
+    // printf("%d\n", ar[1 - 1]);
+    // printf("I am new %d\n", number);
+
+    // /* Second number */
+    // n = pow(10, 2);
+    // snumber = number % n;
+    // ar[2 - 1] = snumber * 10 / n;
+    // number -= snumber;
+    // printf("%d\n", ar[2 - 1]);
+    // printf("I am new %d\n", number);
+
+    /* Third number */
+    n = pow(10, 3);
+    snumber = number % n;
+    printf("I am snumber %d\n", snumber );
+    // ar[3 - 1] = snumber * 10 / n;
+     number -= snumber;
+    printf("Hello%d\n", ar[3 - 1]);
+    // printf("I am new %d\n", number);
+
+    /* Forth number */
+    // n = pow(10, 4);
+    // snumber = number % n;
+    // ar[4 - 1] = snumber * 10 / n;
+    // number -= snumber;
+    // printf("%d\n", ar[4 - 1]);
+
     return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,9 +1,2 @@
-#include <stdio.h>
-int main()
-{
-    printf("I will make a beep\a\n");
-    printf("I am backspace\bremove\n");
-    printf("I am before tab\tI am after tab\n"); // Leaves horizontal space of some characters
-    printf("I terminates a line and moves to next line\n");
-    return 0;
-}
+ // ar[3 - 1] = snumber * 10 / n;
+    // n
