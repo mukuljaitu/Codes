2c2dc6f48e89b7ed6e084a3c92bd94566ba8db38
Mukul Garg <115335617+mukuljaitu@users.noreply.github.com>
2023-05-14 23:32:53 +0530

    Readability improved

--- Lab 6/array_sorting.c
+++ Lab 6/array_sorting.c
@@ -2,7 +2,7 @@
 int main()
 {
     int arr[5] = {0};
-    printf("Enter arr of 5 subjects: ");
+    printf("Enter marks of 5 subjects: ");
     scanf("%d%d%d%d%d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
 
     for (int i = 0; i < 5; i++)
